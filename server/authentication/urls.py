3dad5a3e912b1c8b74d26f749680c13f93e42f5a
foschieraanderson <foschieraanderson@gmail.com>
2020-09-29 23:58:01 -0300

    Alteração no pacote de autenticação e urls

--- server/authentication/urls.py
+++ server/authentication/urls.py
@@ -1,18 +1,20 @@
 from django.urls import path
-from authentication import views
-from .token_response import MyTokenObtainPairView, MyTokenRefreshView
 
+from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
+
+from . import views
 
 app_name = 'authentication'
 
 urlpatterns = [
+    # Endpoint para registro de usuários
     path('register/', views.RegisterView.as_view(), name='register'),
 
-    # rotas protegidas somente para usuários registrados
-    path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', MyTokenRefreshView.as_view(), name='token_refresh'),
+    # Endpoints protegidas para usuários registrados 
+    path('token/', obtain_jwt_token, name='obtain_token'),
+    path('token/refresh/', refresh_jwt_token, name='refresh_token'),
 
-    # rotas protegidas para super usuários (is_staff=True, is_superuser=True)
-    path('users/', views.ListUsersView.as_view(), name='all_users'),
-    path('users/<int:pk>', views.SingleUserView.as_view(), name='single_user')
+    # Endpoints protegidas para super usuários 
+    path('users/', views.ListUsersView.as_view(), name='users-list'),
+    path('users/<int:pk>/', views.DetailUserView.as_view(), name='user-detail'),
 ]
