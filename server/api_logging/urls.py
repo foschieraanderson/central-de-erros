6cddd40d6dd8b28526799b90d1876e47aa14bc4a
foschieraanderson <foschieraanderson@gmail.com>
2020-10-03 00:55:00 -0300

    Inserindo url para levar pra documentação

--- server/api_logging/urls.py
+++ server/api_logging/urls.py
@@ -28,6 +28,9 @@ urlpatterns = [
     path('api/v1/', include('logs.urls')),
     # Endpoints de authenticação e usuários
     path('api/v1/auth/', include('authentication.urls')),
+
+    path('', lambda request: redirect('api/v1/swagger/', permanent=False)),
+
     # Endpoints de Documentação
     path('api/v1/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('api/v1/redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
