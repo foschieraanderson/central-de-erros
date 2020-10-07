87379d66cf99656a38d4267e26e9bad62a513bc5
foschieraanderson <foschieraanderson@gmail.com>
2020-10-07 15:44:17 -0300

    Configurando view de Registro

---
+++ web/src/views/Register.vue
@@ -0,0 +1,33 @@
+<<template>
+    <div>
+        <h2>Register</h2>
+        <form>
+            <div>
+                <label for="email">E-mail</label>
+                <input type="email" name="email" id="email">
+                <div>E-mail é obrigatório</div>
+            </div>
+            <div>
+                <label for="password">Senha</label>
+                <input type="password" name="password" id="password">
+                <div>Senha é obrigatório</div>
+            </div>
+            <div>
+                <button>Entrar</button>
+                <a href="#">Registrar-se</a>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            email:'',
+            password:'',
+            submitted: false
+        }
+    },
+}
+</script>
