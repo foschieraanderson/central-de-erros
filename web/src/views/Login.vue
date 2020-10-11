30d5391b903909f038736e205d277dd34bbce125
foschieraanderson <foschieraanderson@gmail.com>
2020-10-10 23:28:46 -0300

    View de login

--- web/src/views/Login.vue
+++ web/src/views/Login.vue
@@ -1,16 +1,15 @@
-<<template>
+<template>
     <div>
         <h2>Login</h2>
-        <form>
+        <form @submit="handleSubmit">
+            <div>{{ erro }}</div>
             <div>
                 <label for="email">E-mail</label>
-                <input type="email" name="email" id="email">
-                <div>E-mail é obrigatório</div>
+                <input v-model="email" type="email" name="email" id="email">
             </div>
             <div>
                 <label for="password">Senha</label>
-                <input type="password" name="password" id="password">
-                <div>Senha é obrigatório</div>
+                <input v-model="password" type="password" name="password" id="password">
             </div>
             <div>
                 <button>Entrar</button>
@@ -21,13 +20,39 @@
 </template>
 
 <script>
+
+import axios from 'axios';
+
+const baseURL = 'http://127.0.0.1:8000/api/v1/';
+
 export default {
+    name: "Login",
     data() {
         return {
-            email:'',
-            password:'',
+            erro: "",
+            email:"",
+            password:"",
             submitted: false
+        };
+    },
+    methods: {
+        handleSubmit(e) {
+            e.preventDefault();
+
+            const options = {"email": this.email, "password": this.password};
+        
+            axios.post(baseURL + "auth/token/", options)
+            .then(res => {
+                console.log(res.data.token);
+                if (res.data.token) {
+                    localStorage.setItem("user", JSON.stringify(res.data));
+                    this.$router.push('/');
+                }
+            })
+            .catch(err => {
+                this.erro = err.data.email;
+            })
         }
     },
-}
+};
 </script>
