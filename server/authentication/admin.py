d5e31966bf5e6cb5648f74a9d3b2539baaa66e88
foschieraanderson <foschieraanderson@gmail.com>
2020-11-23 23:55:27 -0300

    Removendo arquivos do front

--- server/authentication/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import User
-
-class UserAdmin(admin.ModelAdmin):
-    list_display = ['email', 'username', 'created_at', 'is_staff', 'is_superuser']
-
-admin.site.register(User, UserAdmin)
--- server/logs/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import Log
-
-class LogAdmin(admin.ModelAdmin):
-    list_display = ['title', 'description', 'level', 'origin', 'created_at', 'archived']
-
-admin.site.register(Log, LogAdmin)
