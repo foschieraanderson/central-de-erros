2c0e53e3a9d30bf5da544d4540ab230ece241281
foschieraanderson <foschieraanderson@gmail.com>
2021-03-08 03:42:51 -0300

    Removendo senhas e chaves de acesso

--- app/settings.py
+++ app/settings.py
@@ -91,9 +91,9 @@ WSGI_APPLICATION = 'app.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get('POSTGRES_NAME'),
-        "USER": os.environ.get('POSTGRES_USER'),
-        "PASSWORD": os.environ.get('POSTGRES_PASSWORD'),
+        "NAME": "YOUR_DB_NAME",
+        "USER": "YOUR_DB_USER",
+        "PASSWORD": "YOUR_DB_PASSWORD",
         "HOST": "db",
         "PORT": 5432,
     }
